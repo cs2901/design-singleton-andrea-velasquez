7543ad9b319243467a163a366fd32bbc60b1a8e2
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-05-09 12:24:38 -0500

    Add ChocolateBoiler and SingletonChocolateBoiler class diagrams

---
+++ SingletonChocolateBoiler.java
@@ -0,0 +1,47 @@
+public class SingletonChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private static SingletonChocolateBoiler instance;
+
+    private SingletonChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static SingletonChocolateBoiler getInstance() {
+        if (instance == null){
+            instance = new SingletonChocolateBoiler();
+        }
+        return instance;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(isEmpty() && isBoiled()){
+            //drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && isBoiled()){
+            //bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
